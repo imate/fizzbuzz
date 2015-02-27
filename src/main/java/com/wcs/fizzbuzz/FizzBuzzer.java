2c15fc7ee823f0fd080cfc7318153bdffd84441b
imate <imateee93@gmail.com>
2015-02-27 19:06:51 +0100

    "5-tel osztható" és "15-tel osztható" teszt teljesítve

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -7,6 +7,9 @@ public class FizzBuzzer {
         if (number % 3 == 0) {
             message += "fizz";
         }
+        if (number % 5 == 0) {
+            message += "buzz";
+        }
         return message;
     }
 
